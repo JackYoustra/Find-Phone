801e70cba1c756a3f342e719877404c8b995a457
Jack Youstra <jack@youstra.com>
2015-12-17 13:39:42 -0800

    Make audio state preserved before and after, doesn't discriminate between vibrating and silent.

--- app/src/main/java/com/housemixer/jack/pebblesos/SOSPebbleDataReceiver.java
+++ app/src/main/java/com/housemixer/jack/pebblesos/SOSPebbleDataReceiver.java
@@ -10,6 +10,10 @@ import android.util.Log;
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by jack on 12/15/2015.
  */
@@ -40,12 +44,31 @@ class SOSPebbleDataReceiver extends PebbleKit.PebbleDataReceiver {
     }
 
     public void ring(Context context){
-        AudioManager systemAudio = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
-        systemAudio.setStreamVolume(AudioManager.STREAM_RING, systemAudio.getStreamMaxVolume(AudioManager.STREAM_RING), 0); // notification doesn't work
+        final AudioManager systemAudio = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+        final int maxVolume = systemAudio.getStreamMaxVolume(AudioManager.STREAM_RING);
+        final int currentVolume = systemAudio.getStreamVolume(AudioManager.STREAM_RING);
+        systemAudio.setStreamVolume(AudioManager.STREAM_RING, maxVolume, 0); // notification doesn't work
 
+        // http://stackoverflow.com/questions/4441334/how-to-play-an-android-notification-sound
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Ringtone r = RingtoneManager.getRingtone(context, notification);
+        final Ringtone r = RingtoneManager.getRingtone(context, notification); // should be the same, don't need to worry about getting a different instnace each time
         r.play();
+
+        if (maxVolume != currentVolume) {
+            // not max before, need to change back when done, do this by brute-force play-checking
+            final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+            scheduler.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    if(!r.isPlaying()){
+                        // reset and clean up
+                        systemAudio.setStreamVolume(AudioManager.STREAM_RING, currentVolume, 0);
+                        scheduler.shutdown();
+                    }
+                }
+            }, 1000, 750, TimeUnit.MILLISECONDS);
+            // no communication needed between worker and parent so using thread instead of handler
+        }
     }
 
 }

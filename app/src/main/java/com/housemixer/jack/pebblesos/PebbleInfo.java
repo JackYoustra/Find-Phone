9bb85442907ff848796a8082c697cfd7912f4596
Jack Youstra <jack@youstra.com>
2015-12-22 22:53:10 -0800

    Built in repeated vibration and fart sound

--- app/src/main/java/com/housemixer/jack/pebblesos/PebbleInfo.java
+++ app/src/main/java/com/housemixer/jack/pebblesos/PebbleInfo.java
@@ -8,7 +8,9 @@ import java.util.UUID;
 public class PebbleInfo {
     public static final UUID PEBBLE_APP_UUID = UUID.fromString("16d01f80-dbf6-417b-80e4-83c4506a8339");
     public static final int PEBBLE_APP_RINGCODE_KEY = 0;
-    public static final long PEBBLE_APP_RINGCODE_VALUE = 650;
+    public static final byte PEBBLE_APP_SINGLE_RING_VALUE = 0;
+    public static final byte PEBBLE_APP_TOGGLE_RING_VALUE = 1;
+    public static final byte PEBBLE_APP_FART_VALUE = 2;
 
     private PebbleInfo() {
     }
--- app/src/main/java/com/housemixer/jack/pebblesos/SOSPebbleDataReceiver.java
+++ app/src/main/java/com/housemixer/jack/pebblesos/SOSPebbleDataReceiver.java
@@ -11,6 +11,11 @@ import android.util.Log;
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by jack on 12/15/2015.
  */
@@ -18,6 +23,10 @@ class SOSPebbleDataReceiver extends PebbleKit.PebbleDataReceiver {
 
     public static SOSPebbleDataReceiver instance = null;
     public static MediaPlayer ringPlayer = null;
+    public static MediaPlayer fartPlayer = null;
+    public static boolean isRepeating = false;
+    public static final ScheduledExecutorService repeatingExecutor =  Executors.newSingleThreadScheduledExecutor();
+    public static Future<?> musicRunnableFuture = null;
 
     public static SOSPebbleDataReceiver getInstance(){
         if(instance == null){
@@ -34,10 +43,16 @@ class SOSPebbleDataReceiver extends PebbleKit.PebbleDataReceiver {
     public void receiveData(final Context context, final int transactionId, final PebbleDictionary data) {
         // Log.i(getLocalClassName(), "Received value=" + data.getUnsignedIntegerAsLong(0) + " for key: 0");
         Log.i("PebbleReceptor", "Message Received");
-        long ringcodeMessage = data.getUnsignedIntegerAsLong(PebbleInfo.PEBBLE_APP_RINGCODE_KEY);
-        if (ringcodeMessage == PebbleInfo.PEBBLE_APP_RINGCODE_VALUE) {
-            ring(context);
-
+        byte ringcodeMessage = data.getBytes(PebbleInfo.PEBBLE_APP_RINGCODE_KEY)[0];
+        switch (ringcodeMessage){
+            case PebbleInfo.PEBBLE_APP_SINGLE_RING_VALUE:
+                ring(context);
+                break;
+            case PebbleInfo.PEBBLE_APP_TOGGLE_RING_VALUE:
+                toggleRepeatedRing(context);
+                break;
+            case PebbleInfo.PEBBLE_APP_FART_VALUE:
+                fart(context);
         }
         PebbleKit.sendAckToPebble(context, transactionId);
     }
@@ -67,8 +82,54 @@ class SOSPebbleDataReceiver extends PebbleKit.PebbleDataReceiver {
         // automatically vibrate
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         int vibrationDuration = ringPlayer.getDuration();
-        if (vibrationDuration == -1) vibrationDuration = 3;
+        if (vibrationDuration == -1) vibrationDuration = 2;
         vibrator.vibrate(vibrationDuration);
     }
 
+    public void toggleRepeatedRing(final Context context){
+        if(!isRepeating){
+            musicRunnableFuture = repeatingExecutor.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    ring(context);
+                }
+            }, 0, 2, TimeUnit.SECONDS);
+            isRepeating = true;
+        }
+        else{
+            musicRunnableFuture.cancel(false); // ring seems like a pretty atomically important function
+            isRepeating = false;
+        }
+    }
+
+    // probably some way to make this besides copy/paste, really don't feel like making it orthogonal right now
+    public void fart(Context context) {
+        final AudioManager systemAudio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        final int streamMusic = AudioManager.STREAM_MUSIC;
+        final int currentAudioLevel = systemAudio.getStreamVolume(streamMusic);
+        final int streamMaxVolume = systemAudio.getStreamMaxVolume(streamMusic);
+        systemAudio.setStreamVolume(streamMusic, streamMaxVolume, 0); // notification doesn't work
+
+        if (fartPlayer == null) {
+            fartPlayer = MediaPlayer.create(context, R.raw.fart);
+            fartPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    systemAudio.setStreamVolume(streamMusic, currentAudioLevel, 0);
+                    fartPlayer.release();
+                    fartPlayer = null;
+                }
+            });
+        } else {
+            fartPlayer.seekTo(0);
+        }
+        fartPlayer.start();
+        // no vibration for fart, can change if necessary
+        /*
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        int vibrationDuration = fartPlayer.getDuration();
+        if (vibrationDuration == -1) vibrationDuration = 2;
+        vibrator.vibrate(vibrationDuration);
+        */
+    }
 }
